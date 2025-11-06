ca57511a6b04161f3c07d639bcb5d33fdc20b1d7
Renan Ladislau de Sousa <renanladislau9@gmail.com>
2025-11-06 10:47:01 -0300

    update: adicionando a página de Cadastro do cliente

--- src/AppRoutes.jsx
+++ src/AppRoutes.jsx
@@ -3,6 +3,8 @@ import { Routes, Route } from 'react-router-dom'
 import RedirectionPage from "./pages/RedirectionPage";
 import StartPage from "./pages/StartPage"
 import LoginPage from "./pages/LoginPage";
+import SignInPage from "./pages/SignInPage";
+
 
 function AppRoutes() {
   return (
@@ -10,6 +12,7 @@ function AppRoutes() {
       <Route path="/" element={<StartPage />} />
       <Route path="/redirection" element={<RedirectionPage />} />
       <Route path="/login" element= {<LoginPage/>}/>
+      <Route path="/SignIn" element={<SignInPage/>}/>
 
     </Routes>
   )
---
+++ src/pages/SignInPage.jsx
@@ -0,0 +1,13 @@
+import Styles from "./CSS/SignInPage.module.css"
+
+function SignInPage() {
+  return (
+    <div className={Styles.SignInPage_container}>
+       <div className={Styles.SignInPage_title_container}>
+        <h2>Crie a Sua Conta</h2>
+       </div>
+    </div>
+  )
+}
+
+export default SignInPage
