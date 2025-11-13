e57ac711c295b57586a60c6618c6281efd5bd75c
Renan Ladislau de Sousa <renanladislau9@gmail.com>
2025-11-13 10:09:34 -0300

    feat: Finalizando a barrinha de pesquisa

--- src/components/HomePage/SearchBar.jsx
+++ src/components/HomePage/SearchBar.jsx
@@ -3,8 +3,9 @@ import Styles from "./CSS/SearchBar.module.css"
 function SearchBar() {
   return (
     <div className={Styles.search_bar_container}>
-        <div><img src="" alt="" /></div>
-        <input type="text" name="searchBar" id="" />
+        <div><img src="./Icons/search_icon.png" alt="Icone de Lupa" /></div>
+        <input type="text" name="searchBar" id={Styles.search_input}
+         placeholder="Buscar Barbearia" />
     </div>
   )
 }
