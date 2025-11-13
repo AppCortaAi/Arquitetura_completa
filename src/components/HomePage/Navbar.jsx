7284df65f7d557e9ce7156997ef33bf2f88fb201
Renan Ladislau de Sousa <renanladislau9@gmail.com>
2025-11-12 21:36:26 -0300

    feat: navbar sendo adicionada na página

--- src/components/HomePage/Navbar.jsx
+++ src/components/HomePage/Navbar.jsx
@@ -1,8 +1,11 @@
-import React from 'react'
+import Styles from "./CSS/Navbar.module.css"
 
 function Navbar() {
   return (
-    <div>Navbar</div>
+    <div className={Styles.navbar_container}>
+        <h3>CortaAI</h3>
+        <button><img src="./Icons/profile_icon2.png" alt="Imagem de Perfil" /></button>
+    </div>
   )
 }
 
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,8 +1,10 @@
+import Navbar from "../components/HomePage/Navbar"
 import Styles from "./CSS/HomePage.module.css"
 
 function HomePage() {
   return (
     <div className={Styles.homepage_container}>
+      <Navbar/>
         {/* <h1>Teste</h1> */}
     </div>
   )
